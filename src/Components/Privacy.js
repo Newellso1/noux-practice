16c2594301c35c3f05da033a67cf4049b3821c64
Sam <samnewell91@gmail.com>
2024-05-07 17:30:02 +0100

    changes to opacity
    privacy component

--- src/Components/Privacy.js
+++ src/Components/Privacy.js
@@ -1,6 +1,6 @@
 export default function Privacy() {
   return (
-    <div className="flex flex-col gap-2 md:text-right md:text-sm md:gap-1">
+    <div className="flex flex-col gap-2 md:text-right md:text-sm md:gap-1 opacity-80">
       <p className="font-bold collapse  md:visible">Legal</p>
       <p className="font-bold md:font-normal">Privacy Policy and Cookies</p>
       <p className="font-bold md:font-normal">© Noux Talent 2024</p>
